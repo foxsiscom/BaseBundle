2b541cd2738183b3cf9b05700b5218e61a7d9882
Rodrigo Regis Palmeira <regisbsb@gmail.com>
2015-05-12 23:37:39 -0300

    [ADD]   - classes do projeto

---
+++ Foxsiscom/BaseBundle/Entity/Repository/EntityRepositoryAbstract.php
@@ -0,0 +1,34 @@
+<?php
+namespace Foxsiscom\BaseBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+abstract class EntityRepositoryAbstract extends EntityRepository
+{
+
+    /**
+     *
+     * @param array $criteria
+     * @return QueryBuilder
+     */
+    public function findByCriteria(array $criteria = array())
+    {
+        $qb = $this->createQueryBuilder('e');
+        $qb->select('e');
+
+        foreach ($criteria as $key => $value) {
+            if (! $this->getClassMetadata()->hasField($key) && ! $this->getClassMetadata()->hasAssociation($key)) {
+                continue;
+            } elseif (is_string($value)) {
+                $value = '%' . str_replace(' ', '%', trim($value)) . '%';
+                $qb->andWhere($qb->expr()
+                    ->like("lower(e.$key)", "lower(:$key)"));
+            } else {
+                $qb->andWhere("e.$key = :$key");
+            }
+            $qb->setParameter($key, $value);
+        }
+
+        return $qb;
+    }
+}
---
+++ Foxsiscom/BaseBundle/Service/ServiceAbstract.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Foxsiscom\BaseBundle\Service;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Security\Core\SecurityContext;
+
+abstract class ServiceAbstract
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    protected $validator;
+
+    protected $securityContext;
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
+
+    /**
+     * @DI\InjectParams({
+     *     "entityManager" = @DI\Inject("doctrine.orm.entity_manager")
+     * })
+     */
+    public function setEntityManager($entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        return $this;
+    }
+
+    public function getSecurityContext()
+    {
+        return $this->securityContext;
+    }
+
+    public function getUser()
+    {
+        return $this->getSecurityContext()->getToken()->getUser();
+    }
+
+    /**
+     * @DI\InjectParams({
+     *     "validator" = @DI\Inject("validator")
+     * })
+     */
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+
+        return $this;
+    }
+
+    /**
+     * @return \Symfony\Component\Validator\Validator
+     */
+    public function getValidator()
+    {
+        return $this->validator;
+    }
+
+    /**
+     * @DI\InjectParams({
+     *     "securityContext" = @DI\Inject("security.context")
+     * })
+     */
+    public function setSecurityContext(SecurityContext $securityContext)
+    {
+        $this->securityContext = $securityContext;
+        return $this;
+    }
+
+    public function getManager()
+    {
+        $manager = $this->getDocumentManager();
+        if (empty($manager)) {
+            $manager = $this->getEntityManager();
+        }
+        return $manager;
+    }
+
+    /**
+     * @param mixed $object
+     * @param string[] $groups
+     * @throws ServiceValidationException
+     */
+    protected function validate($object, $groups = array())
+    {
+
+        $validations = $this->getValidator()->validate($object, $groups);
+
+        if (count($validations)) {
+
+            $messages = array();
+            foreach ($validations as $validation) {
+                $messages[] = $validation->getPropertyPath() . ': ' . $validation->getMessage();
+            }
+
+            $message = implode(' ', $messages);
+
+            $exp = new ServiceValidationException($message);
+            $exp->setValidations($validations);
+
+            throw $exp;
+        }
+    }
+
+    /**
+     *
+     * @param array $arrayData
+     * @return array
+     */
+    protected function filterEmpty($arrayData) {
+        return array_filter(
+            $arrayData,
+            function ($var) {
+                return !($var === '');
+            }
+        );
+    }
+
+    /**
+     *
+     * @param ServiceData $sd
+     * @return \Fox\CmsBundle\Service\StaticPageService
+     */
+    public function add(ServiceData $sd)
+    {
+        $object = $sd->get('object');
+        $this->validate($object, array(
+            'registration'
+        ));
+        $manager = $this->getManager();
+        $manager->persist($object);
+        $manager->flush();
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param ServiceData $sd
+     * @return \Fox\CmsBundle\Service\StaticPageService
+     */
+    public function modify(ServiceData $sd)
+    {
+        $object = $sd->get('object');
+        $this->validate($object, array(
+            'edition'
+        ));
+        $manager = $this->getManager();
+        $manager->persist($object);
+        $manager->flush();
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param ServiceData $sd
+     * @return \Fox\CmsBundle\Service\StaticPageService
+     */
+    public function remove(ServiceData $sd)
+    {
+        $document = $sd->get('object');
+        $manager = $this->getManager();
+        $manager->remove($document);
+        $manager->flush();
+
+        return $this;
+    }
+}
