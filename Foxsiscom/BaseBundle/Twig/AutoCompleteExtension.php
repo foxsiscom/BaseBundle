75b20a0fc4779c73f45f56e33d46a4d08fd574eb
Rodrigo Regis Palmeira <regisbsb@gmail.com>
2015-06-26 02:24:59 -0300

    [ADD]   - autocomplete extension

--- Foxsiscom/BaseBundle/Twig/AutoCompleteExtension.php
+++ Foxsiscom/BaseBundle/Twig/AutoCompleteExtension.php
@@ -1,21 +1,25 @@
 <?php
 namespace Foxsiscom\BaseBundle\Twig;
 
-use JMS\DiExtraBundle\Annotation as DI;
-
-/**
-* @DI\Service
-* @DI\Tag("twig.extension")
-*/
-class AutoCompleteExtenxioin extends \Twig_Extension
+class AutoCompleteExtension extends \Twig_Extension
 {
+    /**
+     * @var string
+     */
+    const SKELETON = '<input type="text" name="%s" id="%s" class="%s" %s >';
+
+    const SCRIPT = '<script type="text/javascript">
+        $(function(){
+            autoComplete("#%s","%s");
+        });
+        </script>';
 
     /**
      * @return string
      */
     public function getName()
     {
-        return 'auto_complete_extensions';
+        return 'auto_complete_extension';
     }
 
     /**
@@ -24,12 +28,41 @@ class AutoCompleteExtenxioin extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('auto_complete', array($this, 'createButton'), array('is_safe' => array('html')))
+            new \Twig_SimpleFunction('auto_complete', array($this, 'createInput'), array('is_safe' => array('html')))
         );
     }
 
-    public function createButton()
+    /**
+     * @param string $name
+     * @param string $routeName
+     * @param string $id
+     * @param string $class
+     * @param string[] $attrs
+     * @return string
+     */
+    public function createInput($name, $routeName, $id, $class = "form-control", $attrs = array())
     {
-        return 'aqui vai funfar';
+        $strAttrs = null;
+
+        foreach ($attrs as $key => $value) {
+            $strAttrs .= "{$key}=\"{$value}\" ";
+        }
+
+        $output = null;
+        $output .= sprintf(
+            self::SKELETON,
+            $name,
+            $id,
+            $class,
+            $strAttrs
+        );
+
+        $output .= sprintf(
+            self::SCRIPT,
+            $id,
+            $routeName
+        );
+
+        return $output;
     }
 }
